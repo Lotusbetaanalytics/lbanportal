e6ded0b282684078d528eaba4c6036cd87ef1ff4
judeakinwale <judeakinwale@gmail.com>
2022-02-04 14:07:11 +0100

    Minor changes

--- utils/currentAppraisalDetails.js
+++ utils/currentAppraisalDetails.js
@@ -1,8 +1,9 @@
 const Appraisal = require("../models/Appraisal")
 
-exports.current = async () => {
+const current = async () => {
   const appraisal = await Appraisal.findOne({status: "Started"})
   console.log(appraisal)
   return {appraisal: appraisal, session: appraisal.session, quarter: appraisal.quarter}
 }
 
+module.exports = current

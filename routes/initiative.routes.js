122ba9cd0b65f70ac08f8ecf361e969166b78af0
judeakinwale <judeakinwale@gmail.com>
2022-02-11 06:10:19 +0100

    Comment unused code in initiative routes

--- routes/initiative.routes.js
+++ routes/initiative.routes.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const {
   addInitiative,
   removeInitiative,
-  updateInitiativeWithScore,
+  // updateInitiativeWithScore,
   getInitiatives,
   getStaffInitiatives,
 } = require("../controllers/initiative.controller");
 const { verifyToken } = require("../middlewares/auth.middleware");
 
 router.post("/add", verifyToken, addInitiative); //staff add initiative
-router.patch("/add/:id", verifyToken, updateInitiativeWithScore); //add score to initiative
+// router.patch("/add/:id", verifyToken, updateInitiativeWithScore); //add score to initiative
 
 router.get("/", verifyToken, getInitiatives); //staff initiatives
 router.get("/:id", verifyToken, getStaffInitiatives); //manager get single staff initiatives

b614e262f38404c4afe1350c936f1d64e18a097b
judeakinwale <judeakinwale@gmail.com>
2022-02-22 12:35:53 +0100

    Add routes for updating result convieniently

--- routes/result.routes.js
+++ routes/result.routes.js
@@ -3,8 +3,10 @@ const {
   createResult,
   getAllResult,
   getCurrentResult,
+  updateCurrentResult,
   getQuarterlyResult,
   getCurrentResultByStaffId,
+  UpdateCurrentResultByStaffId,
   getResult,
   updateResult,
   deleteResult
@@ -14,10 +16,12 @@ const { verifyToken, verifyTokenAdmin } = require("../middlewares/auth.middlewar
 router.post("/", verifyToken, createResult); // create a result
 router.get("/", verifyToken, getAllResult); // get all results
 router.get("/current", verifyToken, getCurrentResult); // get current result for autheticated user
+router.patch("/current", verifyToken, updateCurrentResult); // update current result for autheticated user
 router.get("/quarterly", verifyToken, getQuarterlyResult); // get quarterly result for authenticated user
 router.get("/:id", verifyToken, getResult); // get result details by id
 router.patch("/:id", verifyToken, updateResult); // update result details by id
 router.delete("/:id", verifyToken, deleteResult); // delete result by id
 router.get("/staff/:id", verifyTokenAdmin, getCurrentResultByStaffId); // get current result using user id
+router.patch("/staff/:id", verifyTokenAdmin, UpdateCurrentResultByStaffId); // update current result using user id
 
 module.exports = router;

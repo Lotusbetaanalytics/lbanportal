261d4cf61385c9ccbdda54c48c82499b4a7e4dc2
judeakinwale <judeakinwale@gmail.com>
2022-02-25 08:49:29 +0100

    Update accept and reject endpoints

--- routes/result.routes.js
+++ routes/result.routes.js
@@ -20,8 +20,8 @@ router.get("/", verifyToken, getAllResult); // get all results
 router.get("/current", verifyToken, getCurrentResult); // get current result for autheticated user
 router.patch("/current", verifyToken, updateCurrentResult); // update current result for autheticated user
 router.get("/quarterly", verifyToken, getQuarterlyResult); // get quarterly result for authenticated user
-router.get("/reject", verifyToken, rejectCurrentManagerScore); // reject current result's manager's score for authenticated user
-router.get("/accept", verifyToken, acceptCurrentManagerScore); // accept current result's manager's score for authenticated user
+router.patch("/reject", verifyToken, rejectCurrentManagerScore); // reject current result's manager's score for authenticated user
+router.patch("/accept", verifyToken, acceptCurrentManagerScore); // accept current result's manager's score for authenticated user
 router.get("/:id", verifyToken, getResult); // get result details by id
 router.patch("/:id", verifyToken, updateResult); // update result details by id
 router.delete("/:id", verifyToken, deleteResult); // delete result by id

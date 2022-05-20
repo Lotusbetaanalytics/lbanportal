2cc377eaba6c24c7f9d4015a513fda1e8fa9431a
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2022-05-20 09:40:28 +0100

    appraisal

--- routes/appraisalA.js
+++ routes/appraisalA.js
@@ -10,7 +10,7 @@ const { verifyToken } = require("../middlewares/auth");
 
 router.post("/", createAppraisalA); // create an appraisalA
 router.get("/", getAllAppraisalA); // get all appraisalAs
-router.get("/:id", getAppraisalA); // get appraisalA details by id
+// router.get("/:id", getAppraisalA); // get appraisalA details by id
 router.patch("/:id", verifyToken, updateAppraisalA); // update appraisalA by id
 router.delete("/:id", verifyToken, deleteAppraisalA); // delete appraisalA by id
 
